e2e7a99e0be6f330102952dd790e55dc312411ec
matheusned15 <matheusned15@gmail.com>
2024-08-28 16:23:39 -0300

    Project card-generation created

---
+++ card-generation/src/main/java/com/bank/card_generation/CardGenerationApplication.java
@@ -0,0 +1,13 @@
+package com.bank.card_generation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardGenerationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardGenerationApplication.class, args);
+	}
+
+}
---
+++ card-generation/src/test/java/com/bank/card_generation/CardGenerationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bank.card_generation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardGenerationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
