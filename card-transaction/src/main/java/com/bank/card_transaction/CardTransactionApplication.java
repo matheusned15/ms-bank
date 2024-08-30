93b7608b607c93a55b1624a670ce58908891a4e5
matheusned15 <matheusned15@gmail.com>
2024-08-30 19:14:05 -0300

    Project card-transaction created

---
+++ card-transaction/src/main/java/com/bank/card_transaction/CardTransactionApplication.java
@@ -0,0 +1,13 @@
+package com.bank.card_transaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardTransactionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardTransactionApplication.class, args);
+	}
+
+}
