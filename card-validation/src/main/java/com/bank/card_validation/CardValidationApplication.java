ae1f79bc507dd0f2d83f2949c184604e7cf5e2a2
matheusned15 <matheusned15@gmail.com>
2024-08-30 15:48:28 -0300

    Project card-validation created

---
+++ card-validation/src/main/java/com/bank/card_validation/CardValidationApplication.java
@@ -0,0 +1,13 @@
+package com.bank.card_validation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardValidationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardValidationApplication.class, args);
+	}
+
+}
